925c37a6d5f546abab88f166171fc005ac38c6c0
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-28 01:16:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,7 +47,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do 
-    return false if 
+    return false if !source_array
 end
 
 def reduce_to_any_true(source_array)
