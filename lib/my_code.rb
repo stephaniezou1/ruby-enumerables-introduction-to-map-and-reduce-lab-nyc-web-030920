cd5df58759ffac60429f49e307bddad1072203d1
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-28 01:15:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,7 +34,7 @@ def map_to_square(source_array)
   squared_array
 end
 
-def reduce_to_total(source_array, starting_point = nil)
+def reduce_to_total(source_array, starting_point = 0)
   i = 0 
   total = starting_point
   while i < source_array.length do 
